bfff23cef71d10b21edccc021573d45e46176575
Edvinas Jurevicius <ikonitas@gmail.com>
2012-03-10 19:48:34 +0000

    i hope this merged branch works well

--- music/urls.py
+++ music/urls.py
@@ -5,7 +5,7 @@ from basket.views import show_basket
 
 urlpatterns = patterns('',
         url(r'^$', music, name="music"),
-        url(r'^(?P<slug>[\w\-]+)/$', categories_songs, name="categories_songs"),
         url(r'^cart/$',show_basket, name="cart"),
+        url(r'^(?P<slug>[\w\-]+)/$', categories_songs, name="categories_songs"),
 
         )
