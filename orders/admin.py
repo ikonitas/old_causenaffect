b28a45566abcb0216d0cca5945d1cd7612872f95
Edvinas Jurevicius <ikonitas@gmail.com>
2012-04-05 22:05:08 +0100

    do not show orders which ones are not completed

--- orders/admin.py
+++ orders/admin.py
@@ -2,13 +2,14 @@ from orders.models import Order
 from django.contrib import admin
 
 class OrderAdmin(admin.ModelAdmin):
-    #def changelist_view(self, request, extra_context=None):
-    #    if not request.GET.has_key('payment_status__exact'):
-    #        qu = request.GET.copy()
-    #        qu['payment_status__exact'] = 'Completed'
-    #        request.GET = qu
-    #        request.META['QUERY_STRING'] = request.GET.urlencode()
-    #    return super(OrderAdmin,self).changelist_view(request, extra_context=extra_context)
+    def changelist_view(self, request, extra_context=None):
+        if not request.GET.has_key('payment_status__exact'):
+            qu = request.GET.copy()
+            qu['payment_status__exact'] = 'Completed'
+            request.GET = qu
+            request.META['QUERY_STRING'] = request.GET.urlencode()
+        return super(OrderAdmin,self).changelist_view(request, extra_context=extra_context)
+
     list_display = ('order_number','user','purchased_at','payment_status','transaction_id','payer_email','payer_full_name','basket_id','total',)
 
 admin.site.register(Order, OrderAdmin)
